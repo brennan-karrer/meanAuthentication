a7df9cc50bc81e0c7161612f138c0b637df72841
Brennan Karrer <Brennan.Karrer@Evisions.com>
2017-04-18 20:43:34 -0700

    Add in authentication via JWT Token

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../config/database');
 
 // Register
-router.get('/register', (req, res, next) => {
-  res.send('Register');
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({ success: false, msg: 'Failed to register user.' });
+    } else {
+      res.json({ success: true, msg: 'User registered!' });
+    }
+  });
 });
 
 // Authenticate
-router.get('/authenticate', (req, res, next) => {
-  res.send('Authenticate');
+router.post('/authenticate', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if (err) throw err;
+    if (!user) {
+      return res.json({ success: false, msg: 'User not found.' });
+    }
+
+    User.verifyPassword(password, user.password, (err, isMatch) => {
+      if (err) throw err;
+      if (isMatch) {
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // One Week
+        });
+
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            username: user.username
+          } // Build user object so that password is not returned.
+        })
+      } else {
+        return res.json({ success: false, msg: 'Wrong password.' });
+      }
+    });
+  });
 });
 
 // Profile
-router.get('/profile', (req, res, next) => {
-  res.send('Profile');
-});
-
-// Validate
-router.get('/validate', (req, res, next) => {
-  res.send('Validate');
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  res.json({ user: req.user });
 });
 
 module.exports = router;
