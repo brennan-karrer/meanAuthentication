10b7915189dd3b07ab7a1cd48e7f30415f6135a5
Brennan Karrer <Brennan.Karrer@Evisions.com>
2017-04-19 11:17:42 -0700

    Update database config to point to mLab mongoDB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  // database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://admin:admin@ds049466.mlab.com:49466/meanauth'
   secret: 'secret'
 };
