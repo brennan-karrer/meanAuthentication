cf224fcebb33ad9406a6cc28af6e0498724d096c
Brennan Karrer <Brennan.Karrer@Evisions.com>
2017-04-19 09:07:25 -0700

    Logout a user and clear localStorage data

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -32,4 +32,11 @@ export class AuthService {
     this.authToken = token;
     this.user = user;
   }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+
+    localStorage.clear();
+  }
 }
